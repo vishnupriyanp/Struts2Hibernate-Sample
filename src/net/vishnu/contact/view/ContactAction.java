e683e91ecde2151e80187e41455961bf8d35865e
Vishnupriyan <vishnupriyan@inapp.com>
2014-07-17 10:40:06 +0530

    Initial Commit

---
+++ src/net/vishnu/contact/view/ContactAction.java
@@ -0,0 +1,67 @@
+package net.vishnu.contact.view;
+
+import java.util.List;
+
+import net.vishnu.contact.controller.ContactManager;
+import net.vishnu.contact.model.Contact;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class ContactAction extends ActionSupport {
+
+	private static final long serialVersionUID = 9149826260758390091L;
+	private Contact contact;
+	private List<Contact> contactList;
+	private Long id;
+
+	private ContactManager linkController;
+
+	public ContactAction() {
+		linkController = new ContactManager();
+	}
+
+	public String execute() {
+		this.contactList = linkController.list();
+		return SUCCESS;
+	}
+
+	public String add() {
+		try {
+			linkController.add(getContact());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		this.contactList = linkController.list();
+		return SUCCESS;
+	}
+
+	public String delete() {
+		linkController.delete(getId());
+		return SUCCESS;
+	}
+
+	public Contact getContact() {
+		return contact;
+	}
+
+	public List<Contact> getContactList() {
+		return contactList;
+	}
+
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
+
+	public void setContactList(List<Contact> contactsList) {
+		this.contactList = contactsList;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
